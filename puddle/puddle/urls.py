79d2317a1515044837fc3b4e01700c2bdf451927
= <hubert0295@outlook.com>
2023-02-22 21:02:26 +0100

    Complete Project, ready for upgrades

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('inbox/', include('conversation.urls')),
     path('dashboard', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
