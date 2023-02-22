dab57a352b0af061a439c6132624a823adb5ed06
= <hubert0295@outlook.com>
2023-02-22 15:34:15 +0100

    message

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -13,7 +13,7 @@ class Item(models.Model):
     description = models.TextField(blank=True,null=True)
     price = models.FloatField()
     image = models.ImageField(upload_to='item_imagess', blank=True, null=True)
-    is_sold = models.BooleanField()
+    is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
